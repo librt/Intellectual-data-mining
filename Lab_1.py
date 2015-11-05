1bbaee97f4f2b2085bbdaea66c2f47675d50413c
librt <geor.polina@gmail.com>
2015-11-06 01:24:04 +0200

    dellete print

--- Lab_1.py
+++ Lab_1.py
@@ -7,7 +7,6 @@ import codecs
 with codecs.open("D://text.txt", 'r', 'utf-8') as t:
     text = t.read()
     text = text.lower()
-    print(text)
     text = re.sub(u'[^a-zа-яё]', ' ', text)
     print(text)
     with codecs.open("D://text1.txt", 'w', 'utf-8') as f:
