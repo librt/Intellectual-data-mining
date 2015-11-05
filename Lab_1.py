8c0531125684832e548a311205dbb5af1bdf2198
librt <geor.polina@gmail.com>
2015-11-06 01:20:29 +0200

    add

---
+++ Lab_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Полина'
+
+import re
+import codecs
+
+with codecs.open("D://text.txt", 'r', 'utf-8') as t:
+    text = t.read()
+    text = text.lower()
+    print(text)
+    text = re.sub(u'[^a-zа-яё]', ' ', text)
+    print(text)
+    with codecs.open("D://text1.txt", 'w', 'utf-8') as f:
+        f.write(text)
