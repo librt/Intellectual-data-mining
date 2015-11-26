913b73b42a13edeffb58b016285be9f366f8b391
librt <geor.polina@gmail.com>
2015-11-26 22:03:44 +0200

    split words

--- Lab_1.py
+++ Lab_1.py
@@ -3,4 +3,6 @@ __author__ = 'Полина'
 
 import Zipf_law
 
-Zipf_law.filter("text.txt")
+text=Zipf_law.filter("text.txt", "text1.txt")
+
+Zipf_law.freq(text)
--- Zipf_law.py
+++ Zipf_law.py
@@ -4,12 +4,16 @@ __author__ = 'Полина'
 import re
 import codecs
 
-def filter(textname="name"):
-    with codecs.open(textname, 'r', 'utf-8') as t:
+def filter(name, fname):
+    with codecs.open(name, 'r', 'utf-8') as t:
         text = t.read()
         text = text.lower()
         text = re.sub(u'[^a-zа-яё]', ' ', text)
         text = re.sub(r' +', ' ', text)
-        print(text)
-        with codecs.open("textname1.txt", 'w', 'utf-8') as f:
-            f.write(text)
+        #with codecs.open(fname, 'w', 'utf-8') as f:
+        #   f.write(text) //запись в файл
+    return text
+
+def freq(text):
+    for vocab in re.findall(ur'[a-zа-яё]+', text, re.U):
+        print vocab
